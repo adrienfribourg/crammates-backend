36358e985061214219c14c3d9a589b0e340891f3
adrienfribourg <afribourg1503@gmail.com>
2024-06-16 01:44:32 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,17 +6,18 @@ const users = require('./routes/users');
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
-
 // CORS Configuration
 const corsOptions = {
     origin: 'https://crammates.com', // Replace with your frontend domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
 };
+
 app.use(cors(corsOptions));
+
+// Middleware
+app.use(bodyParser.json());
 
 // Connect to MongoDB
 const db = process.env.MONGODB_URI;
@@ -26,6 +27,9 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Routes
 app.use('/api/users', users);
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
