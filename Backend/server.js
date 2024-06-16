6f0057c329eabc5e7a2b215ece477ea735110ffe
adrienfribourg <afribourg1503@gmail.com>
2024-06-16 01:32:58 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,15 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+
+// CORS Configuration
+const corsOptions = {
+    origin: 'https://crammates.com', // Replace with your frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 const db = process.env.MONGODB_URI;
